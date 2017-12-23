abf0453f257cbb9b86bcfc38a7b61437f0ca6af3
Brett Robichaud <brobichaud@digimarc.com>
2017-12-22 16:19:58 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace RedisCacheLoad
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int threadCount = 5;
+			int cacheTimeoutSec = 5;
+
+			if (args.Length >= 1) threadCount = Convert.ToInt16(args[0]);
+			if (args.Length >= 2) cacheTimeoutSec = Convert.ToInt16(args[1]);
+
+			Console.WriteLine("Thread Count: {0}, Cache Timeout Seconds: {1}", threadCount, cacheTimeoutSec);
+
+			// cache something, for a limited time
+			var user = new SdkUser()
+			{
+				Name = "user@digimarc.com",
+				Enabled = true,
+				Id = 23,
+				SecurityKey = "sss",
+				Expires = new DateTime(2014,10,31),
+			};
+			RedisCache.Instance.Set("TestResolverKey", user, TimeSpan.FromSeconds(cacheTimeoutSec));
+
+			var tasks = new List<Task>();
+			for (int loop = 1; loop <= threadCount; loop++)
+			{
+				int loop1 = loop;
+				tasks.Add(Task.Run(async () => { await BlastRedisCache(loop1); }));
+			}
+
+			Task.WaitAll(tasks.ToArray());
+			Logging.WriteLogData("LoadCache.csv", threadCount);
+		}
+
+		private static async Task BlastRedisCache(int instance)
+		{
+			var logData = new List<Logging.LogData>();
+			int loop = 1;
+
+			while (true)
+			{
+				var sw = Stopwatch.StartNew();
+				var user = await RedisCache.Instance.GetAsync<SdkUser>("TestResolverKey");
+				sw.Stop();
+
+				if (user == null) break;
+
+				var data = new Logging.LogData() { Thread = instance, Iteration = loop++, ElapsedMs = sw.ElapsedMilliseconds };
+				logData.Add(data);
+				Console.WriteLine("Thread: {0:d2}, Iteration: {1:d4}, Cached: {2}, Elapsed: {3}ms", data.Thread, data.Iteration, user.Name, data.ElapsedMs);
+			}
+
+			Logging.AddLogData(logData);
+			Console.WriteLine("Thread: {0:d2} exiting, data no longer cached", instance);
+		}
+	}
+}
---
+++ RedisCache.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace RedisCacheLoad
+{
+	public static class RedisCache
+	{
+		internal static readonly Lazy<ConnectionMultiplexer> _cacheService =
+			new Lazy<ConnectionMultiplexer>(() =>
+			{
+				var options = new ConfigurationOptions();
+				options.EndPoints.Add("devmadrasgb.redis.cache.windows.net", 6380);
+				options.Password = "DdHaG0cSwNpd1v5w+pCZd14YYGMSVmpQHjtP4/lzHlY=";
+				options.Ssl = true;
+				//options.AllowAdmin = true;  // needed for FLUSHDB command
+
+				// experimental
+				//options.KeepAlive = 30;
+				options.ConnectTimeout = 15000;
+				options.SyncTimeout = 15000;
+
+				return ConnectionMultiplexer.Connect(options);
+			});
+
+		internal static IDatabase Instance
+		{
+			get { return _cacheService.Value.GetDatabase(); }
+		}
+
+		public static T Get<T>(this IDatabase cache, string key)
+		{
+			return Deserialize<T>(cache.StringGet(key));
+		}
+
+		public static async Task<T> GetAsync<T>(this IDatabase cache, string key)
+		{
+			return Deserialize<T>(await cache.StringGetAsync(key));
+		}
+
+		public static object Get(this IDatabase cache, string key)
+		{
+			return Deserialize<object>(cache.StringGet(key));
+		}
+
+		public static void Set(this IDatabase cache, string key, object value, TimeSpan? expires)
+		{
+			cache.StringSet(key, Serialize(value), expires);
+		}
+
+		static byte[] Serialize(object obj)
+		{
+			if (obj == null) return null;
+
+			var formatter = new BinaryFormatter();
+			using (var stream = new MemoryStream())
+			{
+				formatter.Serialize(stream, obj);
+				return stream.ToArray();
+			}
+		}
+
+		static T Deserialize<T>(byte[] bytes)
+		{
+			if (bytes == null) return default(T);
+
+			var formatter = new BinaryFormatter();
+			using (var stream = new MemoryStream(bytes))
+			{
+				return (T)formatter.Deserialize(stream);
+			}
+		}
+	}
+}
